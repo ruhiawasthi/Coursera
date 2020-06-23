c72904df4890b5e8b2b23ae49ade3c6a625da23c
Ruhi Awasthi <ruhiawasthi93rd@gmail.com>
2020-06-23 13:45:47 +0530

    Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). You should use input to read a string and float() to convert the string to a number

---
+++ Untitled50.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+hours = input('Enter hours :')
+rate  = input('Enter rate :')
+fhr   = float(hours)
+frate = float(rate)
+Pay   = fhr*frate
+print('Pay:', Pay)
+
