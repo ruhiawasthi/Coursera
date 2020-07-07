292bac38ed16b6534c063a98dfdc1a70518271d3
Ruhi Awasthi <ruhiawasthi93rd@gmail.com>
2020-07-08 01:03:23 +0530

    Building our own functions

---
+++ functions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+x=5
+print('Hello')
+def print_lyrics():
+    print("I'm a lumberjack, and i'm okay")
+    print("I sleep all night and i work all day")
+print('yo')
+x=x+2
+print(x)
+
