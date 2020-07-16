5e81692be00a34385276cd90faa5bdaaade83430
Ruhi Awasthi <ruhiawasthi93rd@gmail.com>
2020-07-16 15:45:04 +0530

    Uploading some python code

---
+++ Prefixes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+line="have a nice day"
+line.startswith("have")
+print(line.startswith("have"))
+line.startswith("c")
+print(line.startswith("c"))
+line.startswith("h")
+print(line.startswith("h"))
+
